b1f690cef523f10af022007b31575abd4f0c5a8a
Luis Angel Riaño Calixto <67494191+LuisAngelRianoC@users.noreply.github.com>
2022-02-21 16:32:33 -0600

    v2 all ok, add carousel

--- js/script.js
+++ js/script.js
@@ -6,9 +6,31 @@ $(document).ready(function () {
       $(".nav-bar").removeClass("sticky");
     }
   });
-  //#region
+  //#toggle
   $(".menu-btn").click(function () {
     $(".nav-bar .menu").toggleClass("active");
     $(".menu-btn i").toggleClass("active");
+  });
+
+  // carrousel
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        item: 1,
+        nav: false,
+      },
+      600: {
+        item: 2,
+        nav: false,
+      },
+      1000: {
+        item: 3,
+        nav: false,
+      },
+    },
   });
 });
