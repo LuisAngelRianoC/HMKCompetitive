de32cdee142fb13296b20097f439e2fdede17c23
Luis Angel Riaño Calixto <67494191+LuisAngelRianoC@users.noreply.github.com>
2022-02-22 15:05:07 -0600

    semi-final version
    add footer, scroll up, typed

--- js/script.js
+++ js/script.js
@@ -5,6 +5,24 @@ $(document).ready(function () {
     } else {
       $(".nav-bar").removeClass("sticky");
     }
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  //#slide up
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+  });
+
+  // typing animation cript
+  var typed = new Typed(".typing", {
+    strings: ["Community", "Family", "Spartans!!"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
   });
   //#toggle
   $(".menu-btn").click(function () {
@@ -16,7 +34,7 @@ $(document).ready(function () {
   $(".carousel").owlCarousel({
     margin: 20,
     loop: true,
-    autoplayTimeOut: 2000,
+    autoplayTimeOut: 1000,
     autoplayHoverPause: true,
     responsive: {
       0: {
